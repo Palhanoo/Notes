cc0a40233d01b20ea809d38a5fa31f3b258df9a6
Palhano <bpalhano00@gmail.com>
2021-08-06 17:05:24 -0300

    Projeto semi finalizado

---
+++ src/pages/EditNoteScreen/index.js
@@ -0,0 +1,98 @@
+import React, {useState, useEffect, useLayoutEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
+import {
+    Container,
+    TitleInput,
+    BodyInput,
+    SaveButton,
+    SaveButtonImage,
+    CloseButton,
+    CloseButtonImage
+} from './styles';
+
+export default () => {
+    const navigation = useNavigation();
+    const route = useRoute();
+    const dispatch = useDispatch();
+    const list = useSelector(state => state.notes.list);
+    
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [status, setStatus] = useState('new');
+
+    useEffect(()=> {
+        //se existir index, verifica se existe na lista
+        if(route.params?.key != undefined && list [route.params.key]) {
+            setStatus('edit');//processo de edição
+            setTitle(list[route.params.key].title);
+            setBody(list[route.params.key].body);
+        }
+    }, []);
+
+    useLayoutEffect(()=> {
+        navigation.setOptions({
+            title: status == 'new' ? 'Nova Anotação' : 'Editar Anotação',
+            headerLeft: () => (
+                <CloseButton underlayColor="transparent" onPress={handleCloseButton}>
+                    <CloseButtonImage source={require('../../assets/close.png')} />
+                </CloseButton>
+            ),
+            headerRight: () => (
+                <SaveButton underlayColor="transparent" onPress={handleSaveButton}>
+                    <SaveButtonImage source={require('../../assets/save.png')} />
+                </SaveButton>
+            )
+        })
+    }, [status, title, body]);
+
+    const handleCloseButton = () => {
+        navigation.goBack();
+    }
+
+    const handleSaveButton = () => {
+        if(title != '' && body != '') {
+            if(status == 'edit') {
+                dispatch({
+                    type:'EDIT_NOTE',
+                    payload:{
+                        key: route.params.key,
+                        title,
+                        body
+                    }
+                })
+
+            }else {
+                dispatch({
+                    type: 'ADD_NOTE',
+                    payload:{title,body}
+                })
+            }
+
+            navigation.goBack();
+        } else {
+            alert("Preencha o título e corpo")
+        }
+    }
+    
+    return (
+        <Container>
+            <TitleInput
+                value={title}
+                onChangeText={t=>setTitle(t)}
+                placeholder="Digite o título da anotação"
+                placeholderTextColor="#CCC"
+                autoFocus={true}
+            />
+            <BodyInput
+                value={body}
+                onChangeText={t=>setBody(t)}
+                placeholder="Digite o corpo da anotação"
+                placeholderTextColor="#CCC"
+                multiline={true}
+                textAlignVertical="top"      
+            />
+        </Container>
+    );
+}
---
+++ src/reducers/NotesReducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+    list:[
+        {title: 'Primeira Nota', body:'teste da nota'}
+    ]
+};
+
+export default (state = initialState, action) => {
+    let newList = [...state.list];
+
+    switch(action.type) {
+        case 'ADD_NOTE':
+            newList.push({
+                title: action.payload.title,
+                body:action.payload.body
+            });
+        break;
+        case 'EDIT_NOTE':
+            if(newList[action.payload.key]) {
+                newList[action.payload.key] = {
+                    title: action.payload.title,
+                    body: action.payload.body
+                };
+            }
+        break;
+    }
+
+    return { ...state, list:newList};
+}
